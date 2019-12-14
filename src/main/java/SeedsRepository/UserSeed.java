fe51e92cf0b748c4178099faa7e4f6477c469190
sina <sina.nabavi16@gmail.com>
2019-12-14 07:57:26 +0330

    user seed raw json

---
+++ src/main/java/SeedsRepository/UserSeed.java
@@ -0,0 +1,162 @@
+package SeedsRepository;
+
+public class UserSeed {
+/*[
+    {
+        "_id": "5df4637448653b2b2a783fde",
+            "firstName": "Lilia",
+            "lastName": "Mccall",
+            "email": "chrystalmcleod@zenolux.com",
+            "createdPollIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 1
+        },
+        {
+            "id": 2
+        }
+    ],
+        "invitedPollIds": [
+    ],
+        "createdMeetingIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 1
+        },
+        {
+            "id": 2
+        }
+    ],
+        "invitedMeetingIds": [
+    ]
+    },
+    {
+        "_id": "5df463741c73d1242a32b5ea",
+            "firstName": "Moreno",
+            "lastName": "Avila",
+            "email": "huffphelps@pharmacon.com",
+            "createdPollIds": [
+        {
+            "id": 3
+        },
+        {
+            "id": 4
+        },
+        {
+            "id": 5
+        }
+    ],
+        "invitedPollIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 1
+        },
+        {
+            "id": 2
+        }
+    ],
+        "createdMeetingIds": [
+        {
+            "id": 3
+        },
+        {
+            "id": 4
+        },
+        {
+            "id": 5
+        }
+    ],
+        "invitedMeetingIds": [
+        {
+            "id": 3
+        },
+        {
+            "id": 4
+        },
+        {
+            "id": 5
+        }
+    ]
+    },
+    {
+        "_id": "5df46374decf61cb3288f832",
+            "firstName": "Luisa",
+            "lastName": "Richardson",
+            "email": "mariannesims@comtrail.com",
+            "createdPollIds": [
+        {
+            "id": 6
+        },
+        {
+            "id": 7
+        },
+    ],
+        "invitedPollIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 1
+        },
+        {
+            "id": 3
+        }
+    ],
+        "createdMeetingIds": [
+        {
+            "id": 6
+        },
+    ],
+        "invitedMeetingIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 4
+        },
+        {
+            "id": 5
+        }
+    ]
+    },
+    {
+        "_id": "5df4637421c0e7873d4edbe4",
+            "firstName": "Iva",
+            "lastName": "Gonzalez",
+            "email": "greershaw@kozgene.com",
+            "createdPollIds": [
+    ],
+        "invitedPollIds": [
+        {
+            "id": 3
+        },
+        {
+            "id": 4
+        },
+        {
+            "id": 5
+        }
+    ],
+        "createdMeetingIds": [
+    ],
+        "invitedMeetingIds": [
+        {
+            "id": 0
+        },
+        {
+            "id": 1
+        },
+        {
+            "id": 6
+        }
+    ]
+    }
+    }
+]*/
+}
